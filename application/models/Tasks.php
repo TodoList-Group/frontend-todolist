f476ad8a4c14ac0b21a3b0144fa74ff83e33169c
Kent Huang <kenttttt0@gmail.com>
2017-11-30 21:48:40 -0800

    added crud

--- application/models/Tasks.php
+++ application/models/Tasks.php
@@ -1,16 +1,79 @@
 <?php
 
+/**
+ * Modified to use REST client to get port data from our server.
+ */
+define('REST_SERVER', 'http://backend.local'); // the REST server host
+define('REST_PORT', $_SERVER['SERVER_PORT']); // the port you are running the server on
 
 /*Data model class for tasks.csv table.*/
 class Tasks extends XML_Model {
 
-	public function __construct()
-	{
+	public function __construct() {
 		parent::__construct(APPPATH . '../data/tasks.xml', 'id');
 	}
 
-	function makeCategorizedPanel($tasks)
-	{
+	protected function load() {
+		// load our data from the REST backend
+		$this->rest->initialize(array('server' => REST_SERVER));
+		$this->rest->option(CURLOPT_PORT, REST_PORT);
+		$this->_data = $this->rest->get('/job');
+
+		$dataarr = (array) $this->_data;
+		//var_dump(current($dataarr));
+		$keyf = array();
+
+		foreach (current($dataarr) as $key => $value) {
+			$keyf[] = $key;
+			//var_dump((string)$value);
+		}
+		$this->_fields = $keyf;
+
+		// rebuild the keys table
+		$this->reindex();
+
+	}
+
+	protected function store() {
+	}
+
+	// Retrieve an existing DB record as an object
+	function get($key, $key2 = null) {
+		echo "get called";
+		$this->rest->initialize(array('server' => REST_SERVER));
+		$this->rest->option(CURLOPT_PORT, REST_PORT);
+		return $this->rest->get('/job/' . $key);
+	}
+
+	// Delete a record from the DB
+	function delete($key, $key2 = null) {
+		$this->rest->initialize(array('server' => REST_SERVER));
+		$this->rest->option(CURLOPT_PORT, REST_PORT);
+		$this->rest->delete('/job/' . $key);
+		$this->load(); // because the "database" might have changed
+	}
+
+	// Update a record in the DB
+	function update($record) {
+		echo "update called";
+		$this->rest->initialize(array('server' => REST_SERVER));
+		$this->rest->option(CURLOPT_PORT, REST_PORT);
+		$key = $record->{$this->_keyfield};
+		$retrieved = $this->rest->put('/job/' . $key, $record);
+		$this->load(); // because the "database" might have changed
+	}
+
+	// Add a record to the DB
+	function add($record) {
+		echo "added called";
+		$this->rest->initialize(array('server' => REST_SERVER));
+		$this->rest->option(CURLOPT_PORT, REST_PORT);
+		$key = $record->{$this->_keyfield};
+		$retrieved = $this->rest->post('/job/' . $key, $record);
+		$this->load(); // because the "database" might have changed
+	}
+
+	function makeCategorizedPanel($tasks) {
 		$parms = ['display_tasks' => $this->tasks->getCategorizedTasks()];
 
 		// INSERT the next two lines
@@ -20,56 +83,54 @@ class Tasks extends XML_Model {
 		return $this->parser->parse('by_category', $parms, true);
 	}
 
-
-
-	function getCategorizedTasks()
-	{
+	function getCategorizedTasks() {
 		$undone = array();
-    // extract the undone tasks
-		foreach ($this->all() as $task)
-		{
-			if ($task->status != 2)
+		// extract the undone tasks
+		foreach ($this->all() as $task) {
+			if ($task->status != 2) {
 				$undone[] = $task;
+			}
+
 		}
 
-    // substitute the category name, for sorting
-		foreach ($undone as $task)
+		// substitute the category name, for sorting
+		foreach ($undone as $task) {
 			$task->group = $this->app->group($task->group);
+		}
 
-    // order them by category
+		// order them by category
 		usort($undone, "orderByCategory");
 
-    // convert the array of task objects into an array of associative objects       
-		foreach ($undone as $task)
+		// convert the array of task objects into an array of associative objects
+		foreach ($undone as $task) {
 			$converted[] = (array) $task;
+		}
 
 		return $converted;
 	}
 
-		// provide form validation rules
-	public function rules()
-	{
-	    $config = array(
-	        ['field' => 'task', 'label' => 'TODO task', 'rules' => 'alpha_numeric_spaces|max_length[64]'],
-	        ['field' => 'priority', 'label' => 'Priority', 'rules' => 'integer|less_than[4]'],
-	        ['field' => 'size', 'label' => 'Task size', 'rules' => 'integer|less_than[4]'],
-	        ['field' => 'group', 'label' => 'Task group', 'rules' => 'integer|less_than[5]'],
-	    );
-	    return $config;
+	// provide form validation rules
+	public function rules() {
+		$config = array(
+			['field' => 'task', 'label' => 'TODO task', 'rules' => 'alpha_numeric_spaces|max_length[64]'],
+			['field' => 'priority', 'label' => 'Priority', 'rules' => 'integer|less_than[4]'],
+			['field' => 'size', 'label' => 'Task size', 'rules' => 'integer|less_than[4]'],
+			['field' => 'group', 'label' => 'Task group', 'rules' => 'integer|less_than[5]'],
+		);
+		return $config;
 	}
 }
 
 // return -1, 0, or 1 of $a's category name is earlier, equal to, or later than $b's
-function orderByCategory($a, $b)
-{
-	if ($a->group < $b->group)
+function orderByCategory($a, $b) {
+	if ($a->group < $b->group) {
 		return -1;
-	elseif ($a->group > $b->group)
+	} elseif ($a->group > $b->group) {
 		return 1;
-	else
+	} else {
 		return 0;
+	}
+
 }
-
-
 
 ?>
